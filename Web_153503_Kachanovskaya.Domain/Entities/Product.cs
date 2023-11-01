ae2c03c49de930d01303c125428b309b47213447
Lera <lerakachanovskaya@gmail.com>
2023-11-01 20:29:19 +0300

    Add Product class

--- Web_153503_Kachanovskaya.Domain/Entities/Product.cs
+++ Web_153503_Kachanovskaya.Domain/Entities/Product.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace Web_153503_Kachanovskaya.Domain.Entities
 {
-    internal class Product
+    public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public int CategoryId { get; set; }
+        public int Price { get; set; }
+        public string? ImgPath { get; set; }
+
     }
 }
